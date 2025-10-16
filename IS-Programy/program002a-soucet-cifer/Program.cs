54c2f2793006465716fc7276e6a0c7bf3226ba1d
Tomáš Žižka <tomas.zizka@tul.cz>
2025-10-16 14:01:55 +0200

    #4 Součet cifer - zatím bez součinu

---
+++ IS-Programy/program002a-soucet-cifer/Program.cs
@@ -0,0 +1,55 @@
+﻿string again = "a";
+while (again == "a")
+{
+    Console.Clear();
+    Console.WriteLine("************************************************");
+    Console.WriteLine("*********** Součet a součin cifer  *************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************* Tomáš Žižka **********************");
+    Console.WriteLine("************** 2.10.2025 ***********************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine("************************************************");
+    Console.WriteLine();
+
+    Console.Write("Zadejte celé číslo: ");
+    int number;
+    while (!int.TryParse(Console.ReadLine(), out number))
+    {
+        Console.Write("Nezadali jste celé číslo. Zadejte ho znovu: ");
+    }
+
+    int suma = 0;
+    int numberBackup = number;
+    int digit;
+
+    // Pokud je vstup záporný, tak ho změníme na kladný
+    if (number < 0)
+    {
+        number = -number;
+    }
+
+    while (number >= 10)
+    {
+        digit = number % 10; // určí se nám zbytek
+        number = (number - digit) / 10;
+        Console.WriteLine("Hodnota zbytku = {0}", digit);
+        suma = suma + digit;
+    }
+
+    // musíme poslední cifru vypsat
+    Console.WriteLine("Poslení zbytek = {0}", number);
+
+    // musíme poslední cifru přičíst
+    suma = suma + number;
+
+    Console.WriteLine();
+    Console.WriteLine("Součet cifer čísla {0} je {1}", numberBackup, suma);
+
+    Console.WriteLine();
+    Console.WriteLine("Pro opakování programu stiskněte klávesu a");
+    again = Console.ReadLine();
+
+}
+
+
