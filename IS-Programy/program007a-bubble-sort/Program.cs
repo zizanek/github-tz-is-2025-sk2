e4092b7e8af92e42f762ea4e7c72b072aaad9c61
Tomáš Žižka <tomas.zizka@tul.cz>
2025-11-20 13:41:39 +0100

    #9 Bubble sort - DONE včetně počtu porovnání, výměn a měření času

--- IS-Programy/program007a-bubble-sort/Program.cs
+++ IS-Programy/program007a-bubble-sort/Program.cs
@@ -1,4 +1,5 @@
-﻿
+﻿using System.Diagnostics;
+
 string again = "a";
 while (again == "a")
 {
@@ -58,8 +59,47 @@ while (again == "a")
     {
         myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound+1);
         Console.Write("{0}; ",myRandNumbs[i]);
+    }
 
-    }  
+    Stopwatch myStopwatch = new Stopwatch();
+
+    int compare = 0;  // počet porovnávání
+    int change = 0;   // počet výměn
+
+    myStopwatch.Start();
+    for(int i = 0; i < n -1 ; i++) {
+        // tento cyklus musí zajisti porovnávání dvou soudedních hodnot
+        // musí dále zajistit, aby se zmenšoval počet porovnávaných hodnot
+        for(int j = 0; j < n - i - 1; j++) {
+            if(myRandNumbs[j] > myRandNumbs[j+1]) {
+                int tmp = myRandNumbs[j+1];
+                myRandNumbs[j+1] = myRandNumbs[j];
+                myRandNumbs[j] = tmp;
+                change++;
+            }
+            compare++;
+        }
+    }
+    myStopwatch.Stop();
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("==================================");
+    Console.WriteLine("Seřazené pole: ");
+    for(int i = 0; i < n; i++) {
+        Console.Write("{0}; ", myRandNumbs[i]);
+    }
+
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine($"Počet porovnání: {compare}");
+    Console.WriteLine($"Počet výměn: {change}");
+    Console.WriteLine();
+    Console.WriteLine("Čas seřazení čísel pomocí BS: {0}", myStopwatch.Elapsed);
+
+
+
 
     
     Console.WriteLine();
